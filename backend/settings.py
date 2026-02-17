c133f8beec06af51ae359ba1e27569be80de12ad
sandeepkumar9760 <sandeeproofingsarvice@gmail.com>
2026-02-18 00:14:18 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+
 
 ALLOWED_HOSTS = ['.onrender.com']
 
