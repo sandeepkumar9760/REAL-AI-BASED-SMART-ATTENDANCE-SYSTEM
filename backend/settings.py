d1508f29e925e5c568ef86827aa2a090086e1747
sandeepkumar9760 <sandeeproofingsarvice@gmail.com>
2026-02-18 21:56:52 +0530

    initial commit

--- backend/settings.py
+++ backend/settings.py
@@ -84,13 +84,14 @@ LOGIN_URL = 'login'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'your_db_name',
-        'USER': 'your_db_user',
-        'PASSWORD': 'your_password',
+        'NAME': 'smart_attendance',
+        'USER': 'postgres',
+        'PASSWORD': '123',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
+
 
 
 
